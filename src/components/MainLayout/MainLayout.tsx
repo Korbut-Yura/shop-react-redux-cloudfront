d5fedcef0b94271018b6e5fcc41db75e0b9a2b78
Yury Korbut <korbutyura@gmail.com>
2023-02-17 22:57:10 +0300

    add more ui changes

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -8,7 +8,7 @@ import Box from "@mui/material/Box";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © Yury Korbut "}
+      {"Copyright © Yury Korbut korbutyura@gmail.com "}
       <Link color="inherit" href="https://material-ui.com/" underline="hover">
         My Store
       </Link>{" "}
