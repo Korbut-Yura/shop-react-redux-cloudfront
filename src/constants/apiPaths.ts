bf6715d5f314384ac8259bfea299c00c346c4cce
Yury Korbut <korbutyura@gmail.com>
2023-03-16 15:28:20 +0300

    update api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://21j8cjc4mj.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://omq350zkfg.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://f4ltmk64b8.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
