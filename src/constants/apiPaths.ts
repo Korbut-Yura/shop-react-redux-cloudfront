6f036c70c7ffc168b359a3eaabfb4a3947915f0b
Yury Korbut <korbutyura@gmail.com>
2023-04-07 00:42:46 +0300

    update cart api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://sg592ity5l.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://8ifqnxbub8.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
